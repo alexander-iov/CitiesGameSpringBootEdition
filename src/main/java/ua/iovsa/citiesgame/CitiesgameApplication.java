4346d90d9ad5b3327e596aa3968324b1b6a61098
alexander-iov <sasha.iovsa@gmail.com>
2021-03-24 11:15:36 +0200

    first commit

---
+++ src/main/java/ua/iovsa/citiesgame/CitiesgameApplication.java
@@ -0,0 +1,13 @@
+package ua.iovsa.citiesgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesgameApplication.class, args);
+	}
+
+}
